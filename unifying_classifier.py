497d4af055b8de294e5ed5eb76bcef610b1264c8
maciej.dziubinski <maciej.dziubinskii@dcx.bz>
2018-01-24 21:01:05 +0100

    Fixes to unifying_autoencoder.py + unifying_classifier.py (without experiments)

--- unifying_classifier.py
+++ unifying_classifier.py
@@ -4,11 +4,11 @@ from keras.utils.np_utils import to_categorical
 from keras.regularizers import l2
 
 
-class UnifyingAutoEncoder:
+class UnifyingClassifier:
     def __init__(self, inp_shapes,
-                 unifying_dim=100, num_unifying_layers=1,
+                 unifying_dim=100, num_unifying_layers=3,
                  num_neurons=40, num_layers=3,
-                 num_epochs=10,reg=1e-3):
+                 num_epochs=10, reg=1e-3):
         self.inp_shapes = inp_shapes
         self.unifying_dim = unifying_dim
         self.num_unifying_layers = 1
@@ -19,18 +19,18 @@ class UnifyingAutoEncoder:
         self.num_epochs_so_far = 0
         self._init_model()
 
-    def fit(self, Xs, verbose=1):
+    def fit(self, Xs, ys, verbose=1):
         self.num_epochs_so_far += self.num_epochs
-        self.re_creation_model.fit(
+        self.cls_model.fit(
             x=Xs,
-            y=Xs,
+            y=ys,
             verbose=verbose,
             epochs=self.num_epochs,
         )
         return self
 
-    def unify(self, Xs):
-        return self.unification_model.predict(Xs)
+    def predict(self, Xs):
+        return self.cls_model.predict(Xs)
 
     def _init_model(self):
         # We initialize as many `Input`s as we had elements in `self.inp_shapes`
@@ -62,42 +62,18 @@ class UnifyingAutoEncoder:
             for stratum in unifying_strata
         ]
 
-        # Now, once we got output from the `base_network`, we can re-create our input
-        unifying_strata_after_ae = [
-            Dense(self.unifying_dim, kernel_regularizer=l2(self.reg))(stratum)
-            for stratum in ae_strata
-        ]
-        unifying_strata_after_ae = [
-            ELU()(stratum)
-            for stratum in unifying_strata_after_ae
-        ]
-
         # We need to expand the dimensions to those that we had on input
         output_strata = [
-            Dense(out_shape, kernel_regularizer=l2(self.reg))(stratum)
-            for stratum, out_shape in zip(unifying_strata_after_ae, self.inp_shapes)
+            Dense(1, activation='sigmoid')(stratum)
+            for stratum in ae_strata
         ]
 
         # We'll need this to re-create the input (and check if it makes sense)
-        self.re_creation_model = Model(inputs=inputs, outputs=output_strata)
-        self.re_creation_model.compile(
+        self.cls_model = Model(inputs=inputs, outputs=output_strata)
+        self.cls_model.compile(
             optimizer='adam',
-            loss='mean_squared_error',
+            loss='binary_crossentropy',
         )
-
-        # And now for the unification model
-        self.unification_model = Model(
-            inputs=inputs,
-            outputs=unifying_strata_after_ae,
-        )
-
-        # central_layer_id = 1 + self.num_layers // 2
-        # central_ae_layer = base_network.layers[central_layer_id]
-        # central_strata = [central_ae_layer(inp) for inp in inputs]
-        # self.unification_model = Model(
-        #     inputs=inputs,
-        #     outputs=central_strata,
-        # )
 
     def _create_base_network(self):
         inp = Input(shape=(self.unifying_dim, ))
