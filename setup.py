8c09c6dc466f0d5119d0366ed7d2693d31bf7471
Krzysztof Leszczynski <leszczynski.christopher@gmail.com>
2018-01-25 22:30:00 +0000

    Adding setup.py. Creating python package to let Krzysztof use it in his model easly.

---
+++ setup.py
@@ -0,0 +1,15 @@
+import os
+from setuptools import setup, find_packages
+
+
+
+setup(
+    name = "unifying_autoencoder",
+    version = "0.1",
+    author = "Maciej Dziubinski",
+    author_email = "ponadto@gmail.com",
+    description = ("Autoencoder for different input."),
+    keywords = "machine-learning autoencoder",
+    url = "https://github.com/MTDzi/unifying_autoencoder",
+    packages=find_packages(),
+)
